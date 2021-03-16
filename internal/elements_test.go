8efde26ef9fd73acccd60b443e4bb7c02da5e6f0
Sebastien Binet <binet@cern.ch>
2021-03-16 18:42:55 +0100

    internal: use http.TimeFormat to marshal Time values

--- internal/elements_test.go
+++ internal/elements_test.go
@@ -1,9 +1,11 @@
 package internal
 
 import (
+	"bytes"
 	"encoding/xml"
 	"strings"
 	"testing"
+	"time"
 )
 
 // https://tools.ietf.org/html/rfc4918#section-9.6.2
@@ -30,5 +32,28 @@ func TestMultistatus_Get_error(t *testing.T) {
 		t.Errorf("Multistatus.Get() = %T, expected an *HTTPError", err)
 	} else if httpErr.Code != 423 {
 		t.Errorf("HTTPError.Code = %v, expected 423", httpErr.Code)
+	}
+}
+
+func TestTimeRoundTrip(t *testing.T) {
+	now := Time(time.Now().UTC())
+	want, err := now.MarshalText()
+	if err != nil {
+		t.Fatalf("could not marshal time: %+v", err)
+	}
+
+	var got Time
+	err = got.UnmarshalText(want)
+	if err != nil {
+		t.Fatalf("could not unmarshal time: %+v", err)
+	}
+
+	raw, err := got.MarshalText()
+	if err != nil {
+		t.Fatalf("could not marshal back: %+v", err)
+	}
+
+	if got, want := raw, want; !bytes.Equal(got, want) {
+		t.Fatalf("invalid round-trip:\ngot= %s\nwant=%s", got, want)
 	}
 }
