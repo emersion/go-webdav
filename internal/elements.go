82e82be7604015509c78aeded28f94afa2e34a81
Alexandr Yolkin <yolkin.alex@gmail.com>
2022-04-18 14:27:52 +0300

    - Allow parse etag without quotes

--- internal/elements.go
+++ internal/elements.go
@@ -342,7 +342,8 @@ type ETag string
 func (etag *ETag) UnmarshalText(b []byte) error {
 	s, err := strconv.Unquote(string(b))
 	if err != nil {
-		return fmt.Errorf("webdav: failed to unquote ETag: %v", err)
+		*etag = ETag(b)
+		return nil
 	}
 	*etag = ETag(s)
 	return nil
