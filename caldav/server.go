8c7e7d3647f17dc7e86892e48be57ea881e1c656
Conrad Hoffmann <ch@bitfehler.net>
2024-02-08 16:53:07 +0100

    *: displayname, desription are optional
    Both the displayname and the description can be absent for both
    calendars and address books. If this is the case they should not show
    up in PROPFIND responses as empty string.

--- caldav/server.go
+++ caldav/server.go
@@ -530,9 +530,6 @@ func (b *backend) propFindCalendar(ctx context.Context, propfind *internal.PropF
 		internal.ResourceTypeName: func(*internal.RawXMLValue) (interface{}, error) {
 			return internal.NewResourceType(internal.CollectionName, calendarName), nil
 		},
-		internal.DisplayNameName: func(*internal.RawXMLValue) (interface{}, error) {
-			return &internal.DisplayName{Name: cal.Name}, nil
-		},
 		calendarDescriptionName: func(*internal.RawXMLValue) (interface{}, error) {
 			return &calendarDescription{Description: cal.Description}, nil
 		},
@@ -558,12 +555,16 @@ func (b *backend) propFindCalendar(ctx context.Context, propfind *internal.PropF
 		},
 	}
 
+	if cal.Name != "" {
+		props[internal.DisplayNameName] = func(*internal.RawXMLValue) (interface{}, error) {
+			return &internal.DisplayName{Name: cal.Name}, nil
+		}
+	}
 	if cal.Description != "" {
 		props[calendarDescriptionName] = func(*internal.RawXMLValue) (interface{}, error) {
 			return &calendarDescription{Description: cal.Description}, nil
 		}
 	}
-
 	if cal.MaxResourceSize > 0 {
 		props[maxResourceSizeName] = func(*internal.RawXMLValue) (interface{}, error) {
 			return &maxResourceSize{Size: cal.MaxResourceSize}, nil
